ccee9e3f7ed478183b2d9a056b39fba84176da9d
RLRama <rl.ramiro11@gmail.com>
2023-05-29 17:17:53 -0300

    Reestructure app

---
+++ pages/6_📝_Acerca_del_proyecto.py
@@ -0,0 +1,36 @@
+st.header("Descripción")
+st.markdown(
+    """
+    - Cumplimenta con las distintas consignas y requisitos impuestos por las guías prácticas de '45: Modelos y Simulación de Sistemas'
+    - Permite la operación paramétrica de una simulación de sistema de colas
+    - Año 2023
+    """
+    )
+
+st.header("Académico")
+st.markdown(
+        """
+    - Universidad Nacional de La Rioja
+    - Departamento Académico de Ciencias Exactas, Físicas y Naturales
+    - Ingeniería en Sistemas de Información
+    - 45: Modelos y Simulación de Sistemas
+    """
+    )
+    
+st.header("Nosotros")
+st.markdown(
+        """
+    - Cano Angel Rodrigo | EISI-821
+    - Dominguez Sotomayor Santiago Ismael | EISI-782
+    - Rios Lopez Ramiro Ignacio | EISI-801    
+    """
+    )
+
+st.header("Herramientas utilizadas")
+st.markdown(
+        """
+    - Python
+    - Streamlit
+    - SimPy
+    """
+    )
