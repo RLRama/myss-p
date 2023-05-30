4e43df30bf321877bb820d784ceaf86d984c6938
RLRama <rl.ramiro11@gmail.com>
2023-05-30 00:42:48 -0300

    Sit1Test

--- pages/1_⚙️_Situación_I.py
+++ pages/1_⚙️_Situación_I.py
@@ -69,7 +69,7 @@ def source(env, number, interval, counter):
 def customer(env, name, counter, time_in_bank):
     """Customer arrives, is served and leaves."""
     arrive = env.now
-    print('%7.4f %s: Here I am' % (arrive, name))
+    st.write('%7.4f %s: Here I am' % (arrive, name))
 
     with counter.request() as req:
         patience = random.uniform(MIN_PATIENCE, MAX_PATIENCE)
@@ -80,19 +80,19 @@ def customer(env, name, counter, time_in_bank):
 
         if req in results:
             # We got to the counter
-            print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+            st.write('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
 
             tib = random.expovariate(1.0 / time_in_bank)
             yield env.timeout(tib)
-            print('%7.4f %s: Finished' % (env.now, name))
+            st.write('%7.4f %s: Finished' % (env.now, name))
 
         else:
             # We reneged
-            print('%7.4f %s: RENEGED after %6.3f' % (env.now, name, wait))
+            st.write('%7.4f %s: RENEGED after %6.3f' % (env.now, name, wait))
 
 
 # Setup and start the simulation
-print('Encabezado supuesto')
+st.write('Encabezado supuesto')
 random.seed(RANDOM_SEED)
 env = simpy.Environment()
 
