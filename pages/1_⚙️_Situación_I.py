590ad19a9658c62690a79a0c6d6ced2a33a3afee
RLRama <rl.ramiro11@gmail.com>
2023-05-30 00:45:43 -0300

    Sit1Test

--- pages/1_⚙️_Situación_I.py
+++ pages/1_⚙️_Situación_I.py
@@ -58,16 +58,6 @@ st.markdown(
     """
 )
 
-random.seed(RANDOM_SEED)
-env = simpy.Environment()
-
-if st.button('Simular'):
-    st.write('')
-    st.write(env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, counter)))
-    st.write(env.run())
-else:
-    st.write('')
-
 def source(env, number, interval, counter):
     """Source generates customers randomly"""
     for i in range(number):
@@ -100,9 +90,19 @@ def customer(env, name, counter, time_in_bank):
             # We reneged
             st.write('%7.4f %s: RENEGED after %6.3f' % (env.now, name, wait))
 
+
 # Setup and start the simulation
+random.seed(RANDOM_SEED)
+env = simpy.Environment()
 
 # Start processes and run
 counter = simpy.Resource(env, capacity=1)
 env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, counter))
 env.run()
+
+if st.button('Simular'):
+    st.write('')
+    st.write(env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, counter)))
+    st.write(env.run())
+else:
+    st.write('')
