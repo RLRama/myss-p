46bec4fccdec82595da136a22be1e60dc0e63845
Dominguez Sotomayor Santiago Ismael <santiuni04@gmail.com>
2023-06-02 02:05:06 -0300

    Update 1_⚙️_Situación_I.py

--- pages/1_⚙️_Situación_I.py
+++ pages/1_⚙️_Situación_I.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import random
 import simpy
-import numpy as np
 import pandas as pd
 
 # Configurar página
@@ -58,18 +57,18 @@ st.markdown(
     """
 )
 
-def source(env, number, interval, counter):
+def source(env, number, interval, counter, results):
     """Source generates customers randomly"""
     for i in range(number):
-        c = customer(env, 'Cliente %04d' % i, counter, time_in_bank=12.0)
+        c = customer(env, 'Cliente %04d' % i, counter, time_in_bank=12.0, results=results)
         env.process(c)
         t = random.expovariate(1.0 / interval)
         yield env.timeout(t)
 
-def customer(env, name, counter, time_in_bank):
+def customer(env, name, counter, time_in_bank, results):
     """Customer arrives, is served and leaves."""
     arrive = env.now
-    st.text('| %04.2f | %s | Evento de llegada |' % (arrive, name))
+    results.append((arrive, name, 'Evento de llegada'))
 
     with counter.request() as req:
         patience = random.uniform(MIN_PATIENCE, MAX_PATIENCE)
@@ -80,15 +79,15 @@ def customer(env, name, counter, time_in_bank):
 
         if req in results:
             # Contador
-            st.text('| %04.2f | %s | Esperó %04.2f min |' % (env.now, name, wait))
+            results.append((env.now, name, 'Esperó %04.2f min' % wait))
 
             tib = random.expovariate(1.0 / time_in_bank)
             yield env.timeout(tib)
-            st.text('| %04.2f | %s | Fin de servicio |' % (env.now, name))
+            results.append((env.now, name, 'Fin de servicio'))
 
         else:
             # No se usa porque no hay abandono
-            st.text('%7.4f %s: RENEGED after %6.3f' % (env.now, name, wait))
+            results.append((env.now, name, 'RENEGED after %6.3f' % wait))
 
 # Configurar e iniciar simulación
 random.seed(RANDOM_SEED)
@@ -96,9 +95,13 @@ env = simpy.Environment()
 
 if st.button('Simular'):
     # Iniciar procesos y ejecutar
-    st.text('| Tiempo | N° de cliente | Detalle de evento |')
+    results = []
     counter = simpy.Resource(env, capacity=1)
-    env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, counter))
+    env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, counter, results))
     env.run()
+
+    # Mostrar resultados en una tabla
+    df = pd.DataFrame(results, columns=['Tiempo', 'N° de cliente', 'Detalle de evento'])
+    st.table(df)
 else:
     st.text('')
