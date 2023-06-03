f757a1aa4071b98cdca587f7bbd0ea9448e5433e
RLRama <rl.ramiro11@gmail.com>
2023-06-03 00:38:40 -0300

    Filename fix

--- pages/9_Test.py
+++ pages/9_Test.py
@@ -52,7 +52,7 @@ with st.sidebar:
         min_value=0
     )
     distribution = st.radio(
-    "Distribución a usar para generar los números aleatorios",
+    "Distribución para generación de números aleatorios",
     ('uniforme', 'gaussiana'))
 
 def generate_random_number(interval, distribution):
@@ -68,21 +68,21 @@ def generate_random_number(interval, distribution):
     else:
         raise ValueError("Invalid distribution specified.")
 
-def simulate_mm1_queue(arr_interval, serv_interval, simulation_time, initial_queue_size):
+def simulate_mm1_queue(arrival_rate, service_rate, simulation_time, initial_queue_size):
     event_list = []
     time = 0
     queue_size = initial_queue_size
-    next_arrival_time = generate_random_number(arr_interval, distribution)
+    next_arrival_time = np.random.exponential(scale=1/arrival_rate)
     next_departure_time = np.inf
 
     while time < simulation_time:
         if next_arrival_time < next_departure_time:
             event_type = 'Arrival'
             time = next_arrival_time
-            next_arrival_time += generate_random_number(arr_interval, distribution)
+            next_arrival_time += np.random.exponential(scale=1/arrival_rate)
 
             if queue_size == 0:
-                next_departure_time = time + generate_random_number(serv_interval, distribution)
+                next_departure_time = time + np.random.exponential(scale=1/service_rate)
 
             queue_size += 1
         else:
@@ -94,7 +94,7 @@ def simulate_mm1_queue(arr_interval, serv_interval, simulation_time, initial_que
                 queue_size -= 1
 
             if queue_size > 0:
-                next_departure_time = time + generate_random_number(serv_interval, distribution)
+                next_departure_time = time + np.random.exponential(scale=1/service_rate)
 
         event_list.append({
             'Time': time,
