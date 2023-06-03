887b04701d92351c97368cb51051863314aef24f
RLRama <rl.ramiro11@gmail.com>
2023-06-03 03:45:42 -0300

    Fix sit2

--- pages/2_⚙️_Situación_II.py
+++ pages/2_⚙️_Situación_II.py
@@ -34,7 +34,7 @@ st.markdown(
     """
 )
 
-def add_breaks(queue_df, break_interval, service_interval, break_duration):
+def add_breaks(queue_df, break_interval, serv_interval, break_duration):
     """Adds breaks to the queue dataframe based on the break interval and break duration."""
     for i in range(len(queue_df)):
         if i % break_interval == 0 and i > 0:
@@ -42,12 +42,12 @@ def add_breaks(queue_df, break_interval, service_interval, break_duration):
             queue_df.loc[i, "Clientes en cola"] = len(queue_df) - i
             queue_df.loc[i, "Hora sig. llegada"] = ""
             queue_df.loc[i, "Hora sig. fin de servicio"] = ""
-            for j in range(i, min(i + service_interval[1], len(queue_df))):
+            for j in range(i, min(i + generate_random_number(serv_interval) len(queue_df))):
                 queue_df.loc[j, "Evento"] = "Servicio antes de descanso"
                 queue_df.loc[j, "Clientes en cola"] = len(queue_df) - j
                 queue_df.loc[j, "Hora sig. llegada"] = ""
                 queue_df.loc[j, "Hora sig. fin de servicio"] = ""
-            for k in range(min(i + service_interval[1], len(queue_df)), min(i + service_interval[1] + break_duration, len(queue_df))):
+            for k in range(min(i + generate_random_number(serv_interval) len(queue_df)), min(i + generate_random_number(serv_interval) + break_duration, len(queue_df))):
                 queue_df.loc[k, "Evento"] = "Descanso"
                 queue_df.loc[k, "Clientes en cola"] = len(queue_df) - k
                 queue_df.loc[k, "Hora sig. llegada"] = ""
